e752c0a704c0fe8932de4356156feea8cc5f04b0
ojingabokkumbap <yooheez@naver.com>
2023-04-04 00:02:06 +0900

    230403_23:49

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,17 @@
-function onGeoOk(){
-  alert("find you"+position);
+const weatherContainer = document.querySelector("#weather span:first-child")
+const city = document.querySelector("#weather span:last-child")
+const API_KEY = "70ead2f97344f9d0c1029631a6eb0711";
+
+function onGeoOk(position){
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError(){
