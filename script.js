73fcbfd6f69ffc600b45f918af73a8761f64ae6c
mangocheeze <asdf990515@naver.com>
2023-04-04 16:29:35 +0900

    230404

---
+++ script.js
@@ -0,0 +1,11 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+};
+
+calculator.add(5, 1);
+calculator.minus(5, 1);
